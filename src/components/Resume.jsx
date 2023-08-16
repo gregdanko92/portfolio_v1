56ce7d7d83c91e46073170e4528a9a34e7a0e4ba
Greg Danko <greg.danko@curieai.com>
2023-08-16 10:29:20 -0700

    res change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -16,6 +16,7 @@ function Resume(){
                     <p>Here's a <a 
                     // target="_blank"
                     href='https://docs.google.com/document/d/e/2PACX-1vSWz9mblXoObYCN-4PfE-6MXQyprqocw3Ff3BBh_IW9pwxgVgPWeLVCGNEf1BDXerz3Hp0_csHGeN5R/pub'
+                    // href='https://docs.google.com/document/d/e/2PACX-1vSWz9mblXoObYCN-4PfE-6MXQyprqocw3Ff3BBh_IW9pwxgVgPWeLVCGNEf1BDXerz3Hp0_csHGeN5R/pub'
                     ><b>copy of my resume</b></a> that you can download, I do try to keep it updated on a regular basis, but please reach out for the most current copy over email at gregdanko92@gmail.com </p>
                 </div>
                 
